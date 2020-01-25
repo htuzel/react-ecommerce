ffd3d7f5b1eb50c115996e40b426b5c7c3270422
Hayreddin Tüzel <hayreddintuzel@gmail.com>
2020-01-25 17:57:46 +0300

    menu item and styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Homepage from './Homepage.component';
+import Homepage from './pages/homepage/homepage.component';
+import './App.css'; 
 
 function App() {
   return (
