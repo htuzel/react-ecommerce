6f0e175ad7c8bb4b3bceb4461f83c7eec1947686
Hayreddin Tüzel <hayreddintuzel@gmail.com>
2020-01-25 15:39:13 +0300

    project creation homepage layout

---
+++ src/Homepage.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+const Homepage =  () => (
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        title
+                    </h1>
+                    <span className="subtitle">
+                        subtitle
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Homepage;
